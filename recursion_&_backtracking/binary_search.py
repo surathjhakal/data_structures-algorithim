b559ea205ea1c79d455be8ad27534d28db563722
Surath Jhakal <46605283+surathjhakal@users.noreply.github.com>
2020-06-18 13:35:29 +0530

    Update binary_search.py

--- recursion_&_backtracking/binary_search.py
+++ recursion_&_backtracking/binary_search.py
@@ -1,20 +1,19 @@
-def binary_search(list1,search,low,high):
-    if high>=low:
-        mid = (low + high) // 2
+def binary_search(list1,search):
+    if len(list1)==0:
+        return False
+    else:
+        mid=len(list1)//2
         if list1[mid]==search:
             return True
-        elif list1[mid]<search:
-            return binary_search(list1,search,mid+1,high)
-        elif list1[mid]>search:
-            return binary_search(list1,search,low,mid-1)
-    else:
-        return False
+        else:
+            if list1[mid]>search:
+                return binary_search(list1[:mid],search)
+            else:
+                return binary_search(list1[mid+1:],search)
 
 list1=[2,23,45,65,87,91,98]
-search=92
-low=0
-high=7
-a=binary_search(list1,search,low,high)
+search=91
+a=binary_search(list1,search)
 if a:
     print("number found")
 else:
