dacea8566df2c928eacaf29a71c4087808172d24
Surath Jhakal <46605283+surathjhakal@users.noreply.github.com>
2020-06-18 15:43:25 +0530

    Update bubble_sort.py

--- recursion_&_backtracking/bubble_sort.py
+++ recursion_&_backtracking/bubble_sort.py
@@ -1,15 +1,12 @@
-def bubble(l,n,a):
-    if a<n-1:
-        if l[a] > l[a+1]:
-            l[a], l[a+1] = l[a+1], l[a]
-            bubble(l,n,a+1)
-    elif n>1:
-        bubble(l,n-1,a)
+def bubble_sort(l,n):
+    for i in range(n-1):
+        if l[i] > l[i+1]:
+            l[i], l[i+1] = l[i+1], l[i]
+    if n>1:
+        bubble_sort(l,n-1)
+    else:
+        print(l)
 
-
-
-l=[75,3,2,8,31,1]
+l = [75,3,2,8,31,1]
 n=len(l)
-a=0
-bubble(l,n,a)
-print(l)
+bubble_sort(l,n)
